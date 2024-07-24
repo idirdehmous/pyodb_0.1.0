26056b794ab0ea0ad67b1fa684f963d0b83dfb60
idirdehmous <idirehmous@gmail.com>
2024-07-24 17:04:34 +0200

    Add exceptions

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
-from distutils.core import setup, Extension
+#-*-coding utf-8 -*- 
+from distutils.core import setup ,Extension
 import os  , sys 
 import sysconfig
 from   Cython.Distutils import build_ext
 from   distutils.sysconfig import customize_compiler, get_config_vars
 
+# IMPORT EXCEPTIONS 
+sys.path.insert(0,"./modules"  )
+from exceptions import *  
 
 
+# VERSION 
 __version__="0.1.0" 
+
 
 # COMPILER FLAGS 
 extra_compile_args = sysconfig.get_config_var('CFLAGS').split()
@@ -19,33 +25,50 @@ sfx  =  sysconfig.get_config_var('EXT_SUFFIX')
 # CWD 
 pwd=os.getenv("PWD")
 
-# NAME OF odb lib  & PATH (Written already by cmake )
+# NAME OF odb lib  &  PATH (Written already by cmake )
 odbpath_file="odb_install_dir"
-if os.path.isfile( odbpath_file ):
-   print( "--odb_install_dir file found !" )
-else:
-   print( "--Cmake failed to create the odb_install_dir file. Please check the odb installation !" )
+try:
+   if os.path.isfile( odbpath_file ):
+      print("-"*50)
+      print( "--odb_install_dir file FOUND !" )
+      print(" "  )
+except:
+   Exception
+   raise pyodbInstallError( "--Cmake failed to find '"+odbpath_file+"' file.\n--Please check the odb installation" )
    sys.exit(0)
 
-_file_ =  open( odbpath_file  , "r" )
-odb_install_dir = _file_.readline()  
-   
+try:
+   _file_ =  open( odbpath_file  , "r" )
+   odb_install_dir = _file_.readline()  
+except:
+   FileNotFoundError
+   raise pyodbInstallError("Problem while reading the odb_install_dir file.\nBuilding the module will FAIL !")  
+   sys.exit(0)
 
+
+# PATHS
 odb_env    =odb_install_dir
 libname    ="libodb.so"
 libpath    =odb_env+"/lib/"+libname
 
-if os.path.isfile( libpath ):
-   print( "--{} found !".format( libpath  )    )
-else:
-   print( "--Path to {} not found. Please check the odb installation, or the file ".format( odb_install_dir ) )
-   sys.exit(0)
+try:
+   if os.path.isfile( libpath ):
+      print("-"*50)
+      print( "--{} FOUND !".format( libpath  )    )
+      print(" "  )
+except:
+     Exception 
+     raise pyodbInstallError ("--Path to {} NOT FOUND. Please check the odb installation, or the file ".format( odb_install_dir ) )
+     sys.exit(0)
 
 # SOURCE AND INCLUDE 
 pyc_src=pwd+"/pycc"
 include=pwd+"/include"
 
 
+
+
+# 
 class BuildModule:
     def __init__ (self ,name   ):
         self.name = name
@@ -81,12 +104,13 @@ m3=BuildModule("pyodb_info")
 m4=BuildModule("pyodb")
 
 
-pyodb_io  =m1.Module(  pyc_src+"/io_module.c"   , include , libpath) 
-pyodb_dca =m2.Module(  pyc_src+"/dca_module.c"  , include , libpath) 
-pyodb_info=m3.Module(  pyc_src+"/info_module.c"  , include ,libpath)
-pyodb     =m4.Module(  pyc_src+"/pyodb_module.c" , include ,libpath) 
+pyodb_io  =m1.Module(  pyc_src+"/io_module.c"     , include , libpath) 
+pyodb_dca =m2.Module(  pyc_src+"/dca_module.c"    , include , libpath) 
+pyodb_info=m3.Module(  pyc_src+"/info_module.c"   , include , libpath)
+pyodb     =m4.Module(  pyc_src+"/pyodb_module.c"  , include , libpath) 
 
 # Main pyodb module NAME 
 setup( name="pyodb",version=__version__, description="C/Python interface to access data in ODB1", author="Idir Dehmous", author_email="idehmous@meteo.be", ext_modules=[ pyodb_dca ,  pyodb , pyodb_io, pyodb_info ], cmdclass={"build_ext": NoSuffixBuilder} ,install_requiers=[libpath] )
 
-
+# TERMINé !
+quit()
